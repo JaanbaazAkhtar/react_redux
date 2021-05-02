e3cbdbddfc95251b8fdc9b75500f0a5b5246369f
JaanbaazAkhtar <jaanbaazakhtar786@gmail.com>
2021-05-03 01:06:10 +0530

    reducers added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+<Provider store={createStoreWithMiddleware(reducers)}>
+  <App />
+</Provider>
+, document.getElementById('root'));
 
