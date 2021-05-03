64a029a3487bba327753b6c3578459478ee06c40
JaanbaazAkhtar <jaanbaazakhtar786@gmail.com>
2021-05-04 03:32:44 +0530

    pushing reducers and actions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, {Component} from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>react redux</h1>
-    </div>
-  );
+export default class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>react redux</h1>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
