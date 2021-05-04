9b61895852e1f41b8b8678f661105b7f7e4694e9
JaanbaazAkhtar <jaanbaazakhtar786@gmail.com>
2021-05-05 04:32:24 +0530

    actions and bindactioncreators added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import * as actions from './actions';
+import { bindActionCreatores } from 'redux';
 
+class App extends Component {
 
-export default class App extends Component {
+  componentDidMount(){
+    this.props.moviesList();
+  }
+
   render(){
     return (
       <div className="App">
-        <h1>react redux</h1>
+        {
+          this.props.movies ?
+            this.props.movies.map((item)=>(
+              <div key={item.id}>
+                {item.name}
+              </div>
+            ))
+            : null
+        }
       </div>
     );
-  }
-  
+  }  
 }
 
-export default App;
+function mapStateToProps(state){
+  return{
+    movies: state.movies
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreatores({moviesList}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
